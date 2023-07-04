224d74733b8f37ab74e283823676b2ecf5e732ec
Niranjan Shah <niranjanshah474@gmail.com>
2023-07-04 23:18:26 +1000

    Basic nav setup complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Hello World
+    <main >
+      {/* Navbar */}
+      <section className="min-h-screen">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1>Niranjan Shah</h1>
+          <ul>
+            <li><BsFillMoonStarsFill/></li>
+            <li>< a href="#">Resume</a></li>
+          </ul>
+        </nav>
+      </section>
     </main>
   )
 }
