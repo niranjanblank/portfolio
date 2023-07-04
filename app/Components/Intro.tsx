9491601c375105a7f282bc9f9554cdaa1bf62657
Niranjan Shah <niranjanshah474@gmail.com>
2023-07-05 02:49:46 +1000

    layout for tech created

--- app/Components/Intro.tsx
+++ app/Components/Intro.tsx
@@ -9,25 +9,25 @@ import centralImage from "../../public/central_image.png"
 
 const Intro = () => {
     return (
-        <>
+        <div>
         <div className="text-center p-10"> 
-        <h2 className='text-5xl py-2 text-teal-600 font-medium'>
-          Niranjan Shah
-        </h2>
-        <h3 className="text-2xl py-2">Data Scientist/ Machine Learning Engineer</h3>
-        <p className="text-md py-5 leading-8 text-gray-800">
-          AI enthusiast, currently studying at the University of Canberra. 
-          Skilled in all aspects of Data Science and ML. See my works below.</p>
-      </div>
-      <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
-        <AiFillFacebook/>
-        <AiFillLinkedin/>
-        <AiFillGithub/>
-      </div>
-      <div className=" mx-auto rounded-full w-80 h-80" >
-        <Image src={centralImage} alt="" />
-      </div>
-        </>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium'>
+            Niranjan Shah
+            </h2>
+            <h3 className="text-2xl py-2">Data Scientist/ Machine Learning Engineer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+            AI enthusiast, currently studying at the University of Canberra. 
+            Skilled in all aspects of Data Science and ML. See my works below.</p>
+        </div>
+        <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillFacebook/>
+            <AiFillLinkedin/>
+            <AiFillGithub/>
+        </div>
+        <div className=" mx-auto rounded-full w-80 h-80" >
+            <Image src={centralImage} alt="" />
+        </div>
+        </div>
     )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
 
 import Intro from './Components/Intro'
 import Navbar from './Components/Navbar'
+import Tech from './Components/Tech'
 
 
 export default function Home() {
   return (
     <main className='bg-white px-10' >
-      {/* Navbar */}
-      <section className="min-h-screen">
-      <Navbar/>
-      <Intro/>
+      <section className="min-h-screen flex flex-col">
+        <Navbar/>
+        <div className="flex-grow flex items-center justify-center">
+         <Intro/>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h3 className="text-3xl py-1">Things about me</h3>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            Since the beginning of my journey in the world of Data Science/Machine Learning, I've done lots of projects
+            regarding <span className="text-teal-500"> Data Analysis</span>, <span className="text-teal-500"> Natural Language Processing</span>
+            , <span className="text-teal-500"> Computer Vision</span> and many more.
+          </p>
+          <p className="text-md py-2 leading-8 text-gray-800">
+           I am also interested in playing video games, reading light novels and playing guitar.
+          </p>
+        </div>
+        <Tech/>
       </section>
     </main>
   )
