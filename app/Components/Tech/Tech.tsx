c0fcd73ca3254e3d1a975b7f6d7a52e66e1f6258
Niranjan Shah <niranjanshah474@gmail.com>
2023-07-05 12:38:07 +1000

    Some more tech added

--- app/Components/Tech/Tech.tsx
+++ app/Components/Tech/Tech.tsx
@@ -3,6 +3,8 @@ import react from '../../../public/react.png'
 import pytorch from '../../../public/pytorch.png'
 import fastapi from '../../../public/fastapi.png'
 import r from '../../../public/R.png'
+import scikit from '../../../public/scikit.png'
+import sql from '../../../public/sql.png'
 import { TechProp } from "./interfaces"
 import TechCard from "./TechCard"
 const Tech = () => {
@@ -11,7 +13,9 @@ const Tech = () => {
         {src: react, alt:'react', label:'React.Js'},
         {src: fastapi, alt:'fastapi', label:'FastAPI'},
         {src: pytorch, alt:'pytorch', label:'PyTorch'},
-        {src: r, alt:'r', label:'R'}
+        {src: r, alt:'r', label:'R'},
+        {src: sql, alt:'sql', label:'SQL'},
+        {src: scikit, alt:'scikit', label:'Scikit-learn'}
     ] 
 
     return (
