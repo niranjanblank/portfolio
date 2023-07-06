8e641254bab00a87ed3410221277a983bfca7e0e
Niranjan Shah <niranjanshah474@gmail.com>
2023-07-06 19:23:28 +1000

    used env variable for sending id for form

--- app/Components/Footer/Footer.tsx
+++ app/Components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import ContactForm from "./ContactForm"
 
 
 const Footer = () => {
@@ -6,29 +7,9 @@ const Footer = () => {
             <div className="w-full font-burtons text-sky-500 text-2xl flex flex-col items-center p-3">
                 Info About Me
             </div>
-            <div className="  bg-real-500 rounded-lg py-3 px-5  max-w-xl   flex flex-col">
-                <h2 className="font-bold text-4xl pb-2 dark:text-white">Let's Chat: Unlocking Possibilities</h2>
-                <p className="text-sm dark:text-gray-200" >Need assistance? Don't worry, I'm here to catch your queries like a ninja catches shurikens. Fill out the form below, and let the problem-solving begin!</p>
-                <form className="flex flex-col gap-4 text-black dark:text-gray-200 text-sm mt-4">
-                    <div>
-                        <label className="block ">Full name</label>
-                        <input className="block bg-gray-200 w-full rounded-sm py-2 px-2 text-black" placeholder="Name"/>
-                    </div>
-                    <div>
-                        <label className="block ">Subject</label>
-                        <input className="block bg-gray-200 w-full rounded-sm py-2 px-2 text-black" placeholder="Name"/>
-                    </div>
-                    <div>
-                        <label className="block">Email</label>
-                        <input className="block bg-gray-200 w-full rounded-sm py-2 px-2 text-black" placeholder="Name"/>
-                    </div>
-                    <div>
-                        <label className="block">Message</label>
-                        <textarea className="block bg-gray-200 w-full rounded-sm py-2 px-2 text-black" rows={4} placeholder="What do you want to say"></textarea>
-                    </div>
-                    <button className="bg-gradient-to-r from-cyan-500 to-sky-400 px-4 text-white py-2 rounded-md w-fit flex">Send Email</button>
-                </form>
-            </div>
+            
+            {/* contact form */}
+            <ContactForm/>
         </div>
     )
 }
