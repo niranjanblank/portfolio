57c67a0a2790151f5dda0826e3318eba0e6411f5
Niranjan Shah <niranjanshah474@gmail.com>
2023-07-10 20:55:20 +1000

    transition added for link in portfolio card

--- app/Components/Portfolio/PortfolioItem.tsx
+++ app/Components/Portfolio/PortfolioItem.tsx
@@ -10,8 +10,10 @@ const PortfolioItem:React.FC<PortfolioProp> = (prop) => {
     // </a>
     <div className='shadow-lg rounded-lg p-4 relative
     transition ease-in-out hover:-translate-y-1  hover:scale-105 dark:bg-gray-800'>
+        {/* link for github repo */}
          <a className="absolute text-white right-10 top-10 w-10 h-10 bg-gray-500 rounded-full flex justify-center items-center cursor-pointer
          bg-gradient-to-r from-indigo-500 to-indigo-900
+         transition ease-in-out hover:-translate-y-1  hover:scale-105
          " 
          href={prop.link}
          >
