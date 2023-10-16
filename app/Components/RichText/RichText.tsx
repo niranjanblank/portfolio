d277887ec515e0acb289498c20b4c8384f1c72c0
Niranjan Shah <niranjanshah474@gmail.com>
2023-10-16 22:16:24 +1100

    changes made relating centering image and hyperlink in rich text

--- app/Components/RichText/RichText.tsx
+++ app/Components/RichText/RichText.tsx
@@ -1,5 +1,5 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS, Document, MARKS, Node, Mark } from "@contentful/rich-text-types";
+import { BLOCKS, Document, MARKS, Node, INLINES, Mark } from "@contentful/rich-text-types";
 
 
 
@@ -18,9 +18,8 @@ const options = {
         [BLOCKS.PARAGRAPH]: (node: Node, children: React.ReactNode) => {
             // Use type assertions if needed, but it's generally safer to handle potential undefined/null values
             const contentWithCodeMark = (node as any).content?.find((item: any) => 
-                (item.marks as Mark[]).find(mark => mark.type === MARKS.CODE)
-            );
-
+            item.marks && Array.isArray(item.marks) && (item.marks as Mark[]).find(mark => mark.type === MARKS.CODE)
+        );
             if (contentWithCodeMark) {
                 return (
                     <div >
@@ -38,7 +37,11 @@ const options = {
       
             // Ensure the asset is an image
             if (file.contentType.includes('image')) {
-              return <img src={file.url} alt={description || title} />;
+              return (
+              <div className="flex justify-center">
+                <img src={file.url} alt={description || title} />
+              </div>
+              )
             }
       
             return null; // or a placeholder for non-image assets
@@ -48,7 +51,10 @@ const options = {
         [BLOCKS.HEADING_3]: (node: Node, children: React.ReactNode) => <h3 className="dark:text-white">{children}</h3>,
         [BLOCKS.HEADING_4]: (node: Node, children: React.ReactNode) => <h4 className="dark:text-white">{children}</h4>,
         [BLOCKS.HEADING_5]: (node: Node, children: React.ReactNode) => <h5 className="dark:text-white">{children}</h5>,
-        [BLOCKS.HEADING_6]: (node: Node, children: React.ReactNode) => <h6 className="dark:text-white">{children}</h6>
+        [BLOCKS.HEADING_6]: (node: Node, children: React.ReactNode) => <h6 className="dark:text-white">{children}</h6>,
+        [INLINES.HYPERLINK]: (node: any, children: React.ReactNode) => {
+            return <a href={node.data.uri} target="_blank" className="dark:text-white">{children}</a>;
+        },
     }
 }
 
