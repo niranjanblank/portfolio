ea021f194022f7b83407ab33ebfbef6c56d03123
Niranjan Shah <niranjanshah474@gmail.com>
2024-07-13 20:56:30 +1000

    external cdn added for pdf.worker

--- app/mycv/pdf_render.tsx
+++ app/mycv/pdf_render.tsx
@@ -10,10 +10,7 @@ import { cvDownloadHandler } from "@/app/lib/utils"
 import {useTheme} from 'next-themes'
 
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-  ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PdfRender:React.FC = () => {
   const [numPages, setNumPages] = useState<number>();
