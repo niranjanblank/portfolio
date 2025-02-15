2ff83dbbfe10e8f0924247e83774e65d26c87699
Niranjan <niranjanshah474@gmail.com>
2025-02-16 01:01:58 +1100

    styling added to intro

--- app/Components/Intro/Intro.tsx
+++ app/Components/Intro/Intro.tsx
@@ -37,31 +37,34 @@ const Intro = async () => {
     const intro_data = await getIntro()
    
     return (
-        <div>
-            <div className="mx-auto mt-2 rounded-full w-64 h-64 md:h-96 md:w-96 "  >
-            {/* <Image src={centralImage} alt="" /> */}
-            <Image src={intro_data.profile_image} alt="profile image" 
-            width={96}
-            height={96}
-              unoptimized={true}
-              priority={true}
-            className='rounded-full w-64 h-64 md:h-96 md:w-96 object-cover object-top border-2'/>
-        </div>
-        <div className="text-center p-10  dark:text-white "> 
-            <h2 className='text-5xl py-2 text-sky-500 font-medium md:text-6xl'>
-            Niranjan Shah
-            </h2>
-            <h3 className="text-2xl py-2 md:text-3xl">{intro_data.title}</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-400">
-                {intro_data.desc}
-            </p>
-        </div>
-        <div className="text-5xl flex justify-center gap-16 pb-10 text-gray-600 dark:text-gray-400">
-            <a href="https://www.facebook.com/niranjanBlanK/" className="cursor-pointer"><AiFillFacebook/></a>
-            <a href="https://www.linkedin.com/in/niranjan-shah-7037921a4/" className="cursor-pointer"><AiFillLinkedin/></a>
-            <a href="https://github.com/niranjanblank" className="cursor-pointer"><AiFillGithub/></a>
-        </div>
- 
+        <div className='flex flex-col-reverse justify-center items-center md:flex-row'>
+            <div className='flex flex-col items-center justify-start'>
+                <div className="text-center p-10  dark:text-white "> 
+                    <h2 className='text-5xl py-2 text-sky-500 font-bold md:text-6xl'>
+                    NIRANJAN SHAH
+                    </h2>
+                    <h3 className="text-2xl py-2 md:text-3xl">{intro_data.title}</h3>
+                    <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-400">
+                        {intro_data.desc}
+                    </p>
+                </div>
+                <div className="text-5xl flex justify-center gap-16 pb-10 text-gray-600 dark:text-gray-400">
+                    <a href="https://www.facebook.com/niranjanBlanK/" className="cursor-pointer"><AiFillFacebook/></a>
+                    <a href="https://www.linkedin.com/in/niranjan-shah/" className="cursor-pointer"><AiFillLinkedin/></a>
+                    <a href="https://github.com/niranjanblank" className="cursor-pointer"><AiFillGithub/></a>
+                </div>
+            </div>
+
+            <div className="mx-auto mt-2 rounded-full w-64 h-64 md:h-96 md:w-80 "  >
+                {/* <Image src={centralImage} alt="" /> */}
+                <Image src={intro_data.profile_image} alt="profile image" 
+                width={96}
+                height={96}
+                unoptimized={true}
+                priority={true}
+                className='rounded-full w-64 h-64 md:h-96 md:w-80 object-cover object-top border-2'/>
+            </div>
+     
     </div>
     )
 }
