e993c39d7bf454c8f49792e626c071a967e7c66c
Niranjan Shah <niranjanshah474@gmail.com>
2025-02-19 15:04:14 +1100

    debug color removed

--- app/Components/Intro/Intro.tsx
+++ app/Components/Intro/Intro.tsx
@@ -55,7 +55,7 @@ const Intro = async () => {
                 </div>
             </div>
 
-            <div className="mx-auto mt-2 rounded-full w-64 h-64 md:h-96 md:w-80 lg:w-96 lg:h-[500px] bg-red-500" >
+            <div className="mx-auto mt-2 rounded-full w-64 h-64 md:h-96 md:w-80 lg:w-96 lg:h-[500px]" >
                 {/* <Image src={centralImage} alt="" /> */}
                 <Image src={intro_data.profile_image} alt="profile image" 
                 width={96}
